74826e1fa942e8efc44854e46eace5684ca069d9
George <gio.pircx@yahoo.com>
2020-06-11 17:56:01 +0200

    Updates

---
+++ precache-manifest.0764f84a23dab82339c0a70ebaf22006.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e0607760f738855b6ea94fdc18a9691",
+    "url": "/ShopProject/index.html"
+  },
+  {
+    "revision": "297a5dfd6a306c061edb",
+    "url": "/ShopProject/static/css/main.2d9ec0c0.chunk.css"
+  },
+  {
+    "revision": "b310631d717f47e9503a",
+    "url": "/ShopProject/static/js/2.c4ddb924.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ShopProject/static/js/2.c4ddb924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297a5dfd6a306c061edb",
+    "url": "/ShopProject/static/js/main.34e29f30.chunk.js"
+  },
+  {
+    "revision": "50bf19023c49158da47c",
+    "url": "/ShopProject/static/js/runtime-main.754b546e.js"
+  }
+]);
